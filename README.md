ea4c9abafb767c57a52dd01224503316eb50c567
William-figure <wyc19981230@outlook.com>
2023-04-11 16:51:17 +1000

    First Release of Python

---
+++ README.md
@@ -0,0 +1,4 @@
+#hello-world
+This Repository is going to include all the python files that would be used as personal future study.
+
+
---
+++ hello_world.py
@@ -0,0 +1,16 @@
+#this is a student class
+
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def get_name(self):
+        return self.name
+    def get_age(self):
+        return self.age
+
+if __name__ == '__main__':
+    s1 = Student("William", 12)
+    print(s1.get_name())
+    print(s1.get_age())
